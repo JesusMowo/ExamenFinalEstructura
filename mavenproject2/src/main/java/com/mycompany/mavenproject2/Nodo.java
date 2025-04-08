e4661da74ea384d20e8cc0446daf3656c1bdff14
JesusMowo <alejandrojesusmata@gmail.com>
2025-04-08 17:17:26 -0400

    Segundo Ejercicio

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author aleja
+ */
+public class Nodo {
+    String valor;
+    Nodo siguiente;
+
+    public Nodo(String palabra) {
+        this.valor = palabra;
+        this.siguiente = null;
+    }
+}
