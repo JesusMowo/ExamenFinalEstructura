d86682f877b8b90288b2e6a9836db275c997b938
JesusMowo <alejandrojesusmata@gmail.com>
2025-04-08 17:09:54 -0400

    Primer ejercicio resuelto

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -10,7 +10,16 @@ package com.mycompany.mavenproject2;
  */
 public class Mavenproject2 {
 
+    public static int sumarDesdeFinal(int[] arreglo, int indice) {
+        if (indice < 0) {
+            return 0;
+        }
+        return arreglo[indice] + sumarDesdeFinal(arreglo, indice - 1);
+    }
+
     public static void main(String[] args) {
-        
+        int[] numeros = {5, 7, 2, 9, 5};
+        int resultado = sumarDesdeFinal(numeros, numeros.length - 1);
+        System.out.println("La suma es: " + resultado);
     }
 }
